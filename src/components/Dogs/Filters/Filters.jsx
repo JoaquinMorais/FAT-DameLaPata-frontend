3c5dd4ee6e97de045f076d4f99d79236bce6a90a
JPGenaro <jpgenaro06@gmail.com>
2023-11-16 20:48:55 -0300

    el filtro de nombre funciona

--- src/components/Dogs/Filters/Filters.jsx
+++ src/components/Dogs/Filters/Filters.jsx
@@ -3,12 +3,12 @@ import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Box from '@mui/material/Box';
 
-function Filters({ onNameFilter }) {
+function Filters({ onNameFilter, onToggleOrder }) {
   return (
     <Box display="flex" alignItems="center" justifyContent="center" marginBottom={2} marginTop={2}>
       <Box marginRight={2}>
         <ButtonGroup variant="contained">
-          <Button>Nombre</Button>
+          <Button onClick={onToggleOrder}>Nombre</Button>
           <Button>Edad</Button>
           <Button>Tamaño</Button>
           <Button>Especie</Button>
