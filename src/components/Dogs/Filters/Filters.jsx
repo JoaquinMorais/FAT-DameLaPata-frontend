ea7e614a02bf81cb7aec0244b1476fb07c7c46c7
JPGenaro <jpgenaro06@gmail.com>
2023-11-22 21:35:42 -0300

    diseño de los filtros cmabiado

--- src/components/Dogs/Filters/Filters.jsx
+++ src/components/Dogs/Filters/Filters.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Box from '@mui/material/Box';
 
-function Filters({ onNameFilter, onToggleOrder, onAgeFilter }) {
+function Filters({ onNameFilter, onToggleOrder, onAgeFilter }) {  
+  const buttonStyle = { backgroundColor: 'orange', color: 'white' };
+
   return (
     <Box display="flex" alignItems="center" justifyContent="center" marginBottom={2} marginTop={2}>
-      <Box marginRight={2}>
-        <ButtonGroup variant="contained">
-          <Button onClick={onToggleOrder}>Nombre</Button>
-          <Button onClick={onAgeFilter}>Edad</Button>
-          <Button>Tamaño</Button>
-          <Button>Especie</Button>
+      <Box marginRight={2} >
+        <ButtonGroup>
+          <Button onClick={onToggleOrder} style={buttonStyle}>Nombre</Button>
+          <Button onClick={onAgeFilter} style={buttonStyle}>Edad</Button>
+          <Button style={buttonStyle}>Tamaño</Button>
         </ButtonGroup>
       </Box>
     </Box>
