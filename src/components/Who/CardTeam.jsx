5522e92990194d5b20a2b78ff0273a75f3e7ffa1
Yaco Babiachuk <babiachukyaco@gmail.com>
2023-11-26 22:02:57 -0300

    avaances

--- src/components/Who/CardTeam.jsx
+++ src/components/Who/CardTeam.jsx
@@ -1,30 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { Autoplay, Pagination } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import CartaInd from './CartaInd';
 import styled from 'styled-components';
 
-const StyledTitle = styled.h1`
-  text-align: center;
-  font-size: 32px;
-  color: #333; /* Cambia el color según tus preferencias */
-  margin-bottom: 20px;
+const StyledTitle = styled.div`
+    h1{
+      text-align: center;
+      font-family: 'Raleway', sans-serif;
+      font-weight: 600;
+      color: #333;
+      font-size: 3rem;
+      margin-bottom: 20px;
+    }
+`
+const Wrap = styled.div`
+    background-color: #efefef;
+    padding: 30px;
 `
 
 const StyledPagination = styled.div`
-  display: flex;
-  align-items: center;
+
 `
 
 export default function CardTeam() {
 
   return (
-    <div className='App'>
-      <StyledTitle><h1>EQUIPO DE TRABAJO</h1></StyledTitle>
+    <Wrap>
+      <StyledTitle><h1>Integrantes del Equipo</h1></StyledTitle>
       <div className="container">
         <div className="swiperContainer">
           <Swiper
-            modules={[Pagination, Autoplay]}
+            modules={[Pagination, Autoplay, Navigation]}
             autoplay={{
               delay: 30000,
               disableOnInteraction: false
@@ -33,7 +40,9 @@ export default function CardTeam() {
               el: ".pagination",
               clickable: true,
             }}
+            navigation={true}
             slidesPerView={4}
+            slidesPerGroup={4}
             breakpoints={{
               "@0.00": {
                 slidesPerView: 1,
@@ -60,23 +69,24 @@ export default function CardTeam() {
                 spaceBetween: 20,
               },
             }}
+            
           >
             
-          <SwiperSlide><CartaInd name={'Yaco Ivan Babiachuk'} img={'https://i.postimg.cc/bvhQJWs5/yaco.jpg'}/></SwiperSlide>
-          <SwiperSlide><CartaInd name={'Maximo Tomas Blazquez'} img={'https://i.postimg.cc/PJ9DkFg2/facha.jpg'} /></SwiperSlide>
-          <SwiperSlide><CartaInd name={'Alejo Diaz Broilo'} img={'https://i.postimg.cc/MTZV0B0b/alejo.jpg'} /></SwiperSlide>
-          <SwiperSlide><CartaInd name={'Mateo Emanuel Fernandez'} img={'https://i.postimg.cc/Kz9L0YJX/fercho.jpg'} /></SwiperSlide>
-          <SwiperSlide><CartaInd name={'Marco Fini Minue'} img={'https://i.postimg.cc/c4P7qrZ0/yo.jpg'} /></SwiperSlide>
-          <SwiperSlide><CartaInd name={'Juan Pablo Genaro'} img={'https://i.postimg.cc/Fz7S8QBY/chad.jpg'} /></SwiperSlide>
-          <SwiperSlide><CartaInd name={'Joaquin Morais'} img={'https://i.postimg.cc/mZ5CMtPC/mora.jpg'} /></SwiperSlide>
-          <SwiperSlide><CartaInd name={'Agustin Jose Salonia'} img={'https://i.postimg.cc/3R6Ny0tw/salo.jpg'} /></SwiperSlide>
+          <SwiperSlide><CartaInd name={'Yaco Ivan Babiachuk'} rol={'Frontend Developer'} img={'https://i.postimg.cc/bvhQJWs5/yaco.jpg'}link='https://www.linkedin.com/in/babiachuk-yaco-627bb92a0/'/></SwiperSlide>
+          <SwiperSlide><CartaInd name={'Maximo Tomas Blazquez'} rol={'Quality Assurance'} img={'https://i.postimg.cc/PJ9DkFg2/facha.jpg'} link='https://www.linkedin.com/in/blazquez-m%C3%A1ximo-481154288/'/></SwiperSlide>
+          <SwiperSlide><CartaInd name={'Alejo Diaz Broilo'} rol={'Full Stack Developer'} img={'https://i.postimg.cc/MTZV0B0b/alejo.jpg'} link='https://www.linkedin.com/in/alejo-luis-diaz-broilo-25178029b/'/></SwiperSlide>
+          <SwiperSlide><CartaInd name={'Mateo Emanuel Fernandez'} rol={'Scrum Master'}img={'https://i.postimg.cc/Kz9L0YJX/fercho.jpg'} /></SwiperSlide>
+          <SwiperSlide><CartaInd name={'Marco Fini Minue'} rol={'Frontend Developer'} img={'https://i.postimg.cc/c4P7qrZ0/yo.jpg'} link='https://www.linkedin.com/in/marco-fini-minu%C3%A9-b35289275/'/></SwiperSlide>
+          <SwiperSlide><CartaInd name={'Juan Pablo Genaro'} rol={'Full Stack Developer'} img={'https://i.postimg.cc/Fz7S8QBY/chad.jpg'} link='https://www.linkedin.com/in/juan-pablo-genaro-369a2329b/' /></SwiperSlide>
+          <SwiperSlide><CartaInd name={'Joaquin Morais'} rol={'Team Leader'} img={'https://i.postimg.cc/mZ5CMtPC/mora.jpg'} link='https://www.linkedin.com/in/joaqu%C3%ADn-morais-5a4642220/'/></SwiperSlide>
+          <SwiperSlide><CartaInd name={'Agustin Jose Salonia'} rol={'Frontend Developer'} img={'https://i.postimg.cc/3R6Ny0tw/salo.jpg'} link='https://www.linkedin.com/in/agus-salonia-4b573221b/'/></SwiperSlide>
           </Swiper>
         </div>
         <StyledPagination>
-        <div className="pagination" />
+        <div className="pagination" style={{display:'flex', justifyContent:'center'}}/>
         </StyledPagination>
       </div>
-    </div>
+    </Wrap>
   );
 }
 
--- src/components/Who/CartaInd.jsx
+++ src/components/Who/CartaInd.jsx
@@ -1,48 +1,103 @@
 import React from 'react';
 import 'swiper/css';
 import styled from 'styled-components';
+import LinkedInImage from '../../images/LI-In-Bug.svg';
 
-const StyledCard = styled.div`
+
+const Card = styled.div`
+  margin-top: 10px;
+  width: 250px;
+  border-radius: 25px;
+  background-color: #ff5722;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
+`;
+
+const CardImage = styled.img`
+  position: relative;
+  height: 130px;
+  width: 130px;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 3px solid #fff;
+  padding: 3px;
+`;
+
+const ImageContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 15px 19px;
+  row-gap: 5px;
+`;
+
+const StyledName = styled.text`
+  font-size: 18px;
+  font-weight: 500;
+  color: #333;
+  display: flex;
+  text-align: center;
+`;
+
+const StyledBtn = styled.div`
+  padding: 8px 16px;
+  img{
+    height:40px;
+    width:40px;
+    object-fit: cover;
+    cursor: pointer;
+  }
+`;
+const StyledRol =styled.text`
+  font-size: 14px;
+  color: #707070;
+  text-align: center;
+  padding: 3px;
+`;
+
+const CardContent = styled.div`
+  width: 100%;
+  border-radius: 25px;
+  background-color: #fff;
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 10px 14px;
-`;
-
-const StyledImage = styled.img`
-    height: 100px;
-    width: 100px;
-    object-fit: cover;
-    border-radius: 50%;
-    border: 3px solid #ff5722;
-`;
-
-const ImageContent = styled.div`
-
 `
 
-const StyledName = styled.div`
+export default function CartaInd({ name, img, rol, link, swiperInstance }) {
+  const handleButtonClick = () => {
+    window.open(link, '_blank');
+  };
 
-`
-
-export default function CartaInd({ name, img, swiperInstance }) {
   return (
-    <div className="swiper-slide" style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontSize: '18px',
-      fontWeight: 'bold',
-      paddingBottom: '20px',
-      width: '100%',
-    }}>
-      <StyledCard>
+    <div
+      className="swiper-slide"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '18px',
+        fontWeight: 'bold',
+        paddingBottom: '20px',
+        width: '100%',
+      }}
+    >
+      <Card>
         <ImageContent>
-          <StyledImage src={img} alt={name} />
+          <CardImage src={img} />
         </ImageContent>
-       <StyledName>{name}</StyledName>
-      </StyledCard>
+        <CardContent>
+          <StyledName>{name}</StyledName>
+          <StyledRol>{rol}</StyledRol>
+          <StyledBtn onClick={handleButtonClick}>
+            <img src={LinkedInImage} alt="" />
+          </StyledBtn>
+        </CardContent>
+      </Card>
     </div>
   );
 }
